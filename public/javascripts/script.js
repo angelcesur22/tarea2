860fe5238f0dd3b00eb593f701815c19655951fd
angelcesur22 <149780300+angelcesur22@users.noreply.github.com>
2024-10-14 09:08:50 +0200

    Add files via upload

---
+++ public/javascripts/script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginBtn').addEventListener('click', () => {
+    document.getElementById('loginForm').style.top = '0';
+  });
+  
+  document.getElementById('closeBtn').addEventListener('click', () => {
+    document.getElementById('loginForm').style.top = '-100%';
+  });
+          
